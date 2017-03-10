53194ad822aec7fd5f2080fec6102e775a7c0353
ikosin <jun62626@gmail.com>
2017-03-10 14:16:50 +0900

    add functions to the heat map
    - show AP name, and assoc count

--- public/droidkaigi2017/js/app.js
+++ public/droidkaigi2017/js/app.js
@@ -21,11 +21,13 @@
   function getDataPoints(group) {
     var dataPoints = [];
     Object.keys(group).forEach(function (key) {
-      var associations = getAssociations(key) * (1000 / group[key].max);
+      var associations = getAssociations(key);
       var clonedDataPoint = (JSON.parse(JSON.stringify(_dataPoint)));
       clonedDataPoint.x = group[key].coordinates.x;
       clonedDataPoint.y = group[key].coordinates.y;
-      clonedDataPoint.value = associations;
+      clonedDataPoint.key = key;
+      clonedDataPoint.rowValue = associations;
+      clonedDataPoint.value = associations * (1000 / group[key].max);
       dataPoints.push(clonedDataPoint);
     });
     return dataPoints;
@@ -72,6 +74,47 @@
       data: dataPoints
     };
     heatmapInstance.setData(data);
+
+    // add new
+    var tipCanvas = document.getElementById("tip");
+    var tipCtx = tipCanvas.getContext("2d");
+
+    var c = document.getElementsByClassName("heatmap-canvas")[0];
+    var ctx = c.getContext("2d");
+    Object.keys(dataPoints).forEach(function (i) {
+      var dataPoint = dataPoints[i];
+      ctx.beginPath();
+      ctx.arc(dataPoint.x, dataPoint.y, 5, 0, 2 * Math.PI);
+      ctx.strokeStyle = "#D7000F";
+      ctx.stroke();
+    });
+    c.onmousemove = function(e) {
+      var skip = skip || 0;
+      if (skip++ % 3) return;
+      var hit = false;
+      var self = this;
+      Object.keys(dataPoints).forEach(function (i) {
+
+        var dataPoint = dataPoints[i];
+        ctx.beginPath();
+        ctx.arc(dataPoint.x, dataPoint.y, 5, 0, 2 * Math.PI);
+
+        var rect = self.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+
+        if(ctx.isPointInPath(x, y)) {
+          tipCanvas.style.left = (x) + "px";
+          tipCanvas.style.top = (y - 20) + "px";
+          tipCtx.clearRect(0, 0, tipCanvas.width, tipCanvas.height);
+          tipCtx.font = "12px 'ＭＳ Ｐゴシック'";
+          tipCtx.fillStyle = "black";
+          tipCtx.fillText(dataPoint.key + ": {\"assoc\"," + dataPoint.rowValue + "}", 5, 10);
+          hit = true;
+        }
+      });
+      if (!hit) { tipCanvas.style.left = "-200px"; }
+    }
 
     updateTime();
 
